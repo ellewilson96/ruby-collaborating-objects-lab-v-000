df8695b8231fb871d95a9c408be093009f23e50a
Elle Wilson <eleanorwilsonn@gmail.com>
2018-03-01 22:28:03 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,17 +7,21 @@ class Artist
     @name = name
     @songs = []
   end
+  
+  def self.all
+    @@all
+  end
 
+  def add_song(song)
+    @songs << song
+  end
+  
   def save
     @@all << self
   end
 
   def songs
     @songs
-  end
-
-  def add_song(song)
-    @songs << song
   end
 
   def self.find_or_create_by_name(name)
@@ -30,9 +34,9 @@ class Artist
 		end
 	end
 
-def self.all
-  @@all
-end
+  def self.create(name)
+    self.new(name).tap {|artist| artist.save}
+  end
 
 def print_songs
   puts self.songs
